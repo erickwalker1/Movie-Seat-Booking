ad4eba4d1cff0a2afc04887d3c0bbcc55e68f4bc
RectifierDotErik <86714198+RectifierDotErik@users.noreply.github.com>
2021-07-09 14:39:33 -0400

    Additional Functionality

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 "use strict";
-const seat = document.querySelector(".seat-container");
+const seatContainer = document.querySelector(".seat-container");
 const numSeats = document.getElementById("seat-count");
 const seatPrice = document.getElementById("seat-price");
+
+// Grab all of the seats that are NOT occupied
+const seats = document.querySelectorAll(".row .seat:not(.seat-occupied)");
 
 const movie = document.getElementById("movie");
 const movieOne = document.getElementById("quietPlace");
 const movieTwo = document.getElementById("foreverPurge");
 const movieThree = document.getElementById("zola");
 
-// Listener For Changes In Drop Down Menu
-movie.addEventListener("change", function (event) {
-  seatPrice.innerHTML = event.target.value * numSeats.innerHTML;
-});
+// Save Movie Price To Local Storage
+function setMovieData(moviePrice) {
+  localStorage.setItem("Movie Price", moviePrice);
+}
 
-// Selects Seats 
-seat.addEventListener("click", function (event) {
-  if (
-    !event.target.classList.contains("seat-occupied") &&
-    !event.target.classList.contains("row") &&
-    !event.target.classList.contains("seat-selected")
-  ) {
-    numSeats.innerHTML++;
-    event.target.classList.add("seat-selected");
-  } else if (event.target.classList.contains("seat-selected")) {
-    event.target.classList.remove("seat-selected");
-    numSeats.innerHTML--;
-  }
-  checkMovie();
-});
+// Grabs the seats selected by the user and sets them to an array which is then mapped through and returns the specific index of the selected seat and stores it in Local Storage
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat-selected");
 
-// Update Prices According To Movie Selected
+  const seatIndex = [...selectedSeats].map(function (seat) {
+    return [...seats].indexOf(seat);
+  });
+
+  // seatIndex is an array so set it to a string before saving to local storage
+  localStorage.setItem("Selected Seat", JSON.stringify(seatIndex));
+
+  console.log(seatIndex);
+}
+
+// Update Price Of Tickets According To Movie Selected
 function checkMovie() {
   if (movieOne.selected) {
     seatPrice.innerHTML = movieOne.value;
@@ -45,13 +46,37 @@ function checkMovie() {
   }
 }
 
-/* 
+// Listener For Changes In Drop Down Menu And Saving That Movies' Value To localStorage
+movie.addEventListener("change", function (event) {
+  setMovieData(event.target.value);
+  seatPrice.innerHTML = event.target.value * numSeats.innerHTML;
+});
 
-Task List 
+// Selects Seats
+seatContainer.addEventListener("click", function (event) {
+  if (
+    !event.target.classList.contains("seat-occupied") &&
+    !event.target.classList.contains("row") &&
+    !event.target.classList.contains("seat-selected")
+  ) {
+    numSeats.innerHTML++;
+    event.target.classList.add("seat-selected");
+  } else if (event.target.classList.contains("seat-selected")) {
+    event.target.classList.remove("seat-selected");
+    numSeats.innerHTML--;
+  }
+  updateSelectedCount();
+  checkMovie();
+});
 
- 
- --> Implement functionality to where if user decides to switch movies, update/reflect the price of the tickets (according to amount of seats selected) according to the movie 
+/* Learning Curve Moments
 
- --> Implement functionality to where selected seats are saved to localStorage 
+Grabbing elements on the page ( the 'seats' (divs)) and setting them into an array to be mapped through and returning the index of a certain seat and storing it into localStorage
+  --> Spread Operator 
+  --> having to 'map' through an array 
+  --> Revisiting indexOf() that returns the index of a given element in the array
+  --> Revisiting localStorage and how to store data in it
+
+Listening for a 'change' event in terms of the option select dropdown and storing that specific movies' value inside local storage
 
 */
